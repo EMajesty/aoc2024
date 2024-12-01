6cc66203d39d8a3f51a3b3bc5eb348b3e16e48a9
EMajesty <lassi.laitinen@protonmail.com>
2024-12-01 13:56:38 +0200

    day1_1 done

--- src/bin/aoc1_1.rs
+++ src/bin/aoc1_1.rs
@@ -1,9 +1,35 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::io::BufReader;
 
 fn main() -> std::io::Result<()> {
-    let mut file = File::open("input/input1_1")?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
+    let file = File::open("input/input1")?;
+    let reader = BufReader::new(file);
+
+    let mut result: i32 = 0;
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+
+    for line in reader.lines() {
+        let numbers: Vec<i32> = line
+            .unwrap()
+            .split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect();
+
+        left.push(numbers[0]);
+        right.push(numbers[1]);
+    }
+
+    left.sort();
+    right.sort();
+
+    for i in 0..left.len() {
+        let sum = left[i] - right[i];
+        result += sum.abs();
+    }
+
+    println!("{}", result);
+
     Ok(())
 }
